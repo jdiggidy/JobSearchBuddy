dbc8fe3694854f561c1289a72d0fd7ed3f35f90e
Jeremy Barger <jeremyjusticebarger@gmail.com>
2025-08-25 16:43:28 -0600

    Initial Commit

---
+++ JobSearchBuddy/JobSearchBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchBuddyApp.swift
+//  JobSearchBuddy
+//
+//  Created by Jeremy Barger on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
